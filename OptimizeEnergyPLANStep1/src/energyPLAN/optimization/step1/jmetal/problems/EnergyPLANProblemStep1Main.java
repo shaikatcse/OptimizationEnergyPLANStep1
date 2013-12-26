adb1d8e61a0b7fb17b268dbb0a189b752cb52ec8
shaikatcse@gmail.com <shaikatcse@gmail.com>
2013-12-26 12:27:21 +0100

    Simple autometic multi-run is implmented using a simple loop around the algorithm execution. The var and fun file is renamed as an example run_1_VAR, run_1_FUN.

--- OptimizeEnergyPLANStep1/src/energyPLAN/optimization/step1/jmetal/problems/EnergyPLANProblemStep1Main.java
+++ OptimizeEnergyPLANStep1/src/energyPLAN/optimization/step1/jmetal/problems/EnergyPLANProblemStep1Main.java
@@ -19,20 +19,18 @@ import jmetal.util.JMException;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 
+public class EnergyPLANProblemStep1Main {
 
-public class EnergyPLANProblemStep1Main{
-	
-	  public static Logger      logger_ ;      // Logger object
-	  public static FileHandler fileHandler_ ; // FileHandler object
+	public static Logger logger_; // Logger object
+	public static FileHandler fileHandler_; // FileHandler object
 
 	public static void main(String[] args) throws JMException,
 			SecurityException, IOException, ClassNotFoundException {
 
-		logger_      = Configuration.logger_ ;
-	    fileHandler_ = new FileHandler("NSGAII_main.log"); 
-	    logger_.addHandler(fileHandler_) ;
+		logger_ = Configuration.logger_;
+		fileHandler_ = new FileHandler("NSGAII_main.log");
+		logger_.addHandler(fileHandler_);
 
-		
 		Problem problem; // The problem to solve
 		Algorithm algorithm; // The algorithm to use
 		Operator crossover; // Crossover operator
@@ -42,51 +40,56 @@ public class EnergyPLANProblemStep1Main{
 		HashMap parameters; // Operator parameters
 
 		QualityIndicator indicators; // Object to get quality indicators
-		
-		indicators = null ;	
-		
-		problem=new EnergyPLANProblemStep1("ArrayReal");
+
+		indicators = null;
+
+		problem = new EnergyPLANProblemStep1("ArrayReal");
 		algorithm = new NSGAII(problem);
-	    //algorithm = new ssNSGAII(problem);
+		// algorithm = new ssNSGAII(problem);
 
-	    // Algorithm parameters
-	    algorithm.setInputParameter("populationSize",100);
-	    algorithm.setInputParameter("maxEvaluations",3000);
+		// Algorithm parameters
+		algorithm.setInputParameter("populationSize", 100);
+		algorithm.setInputParameter("maxEvaluations", 3000);
 
-	    // Mutation and Crossover for Real codification 
-	    parameters = new HashMap() ;
-	    parameters.put("probability", 0.9) ;
-	    parameters.put("distributionIndex", 20.0) ;
-	    crossover = CrossoverFactory.getCrossoverOperator("SBXCrossover", parameters);                   
+		// Mutation and Crossover for Real codification
+		parameters = new HashMap();
+		parameters.put("probability", 0.9);
+		parameters.put("distributionIndex", 20.0);
+		crossover = CrossoverFactory.getCrossoverOperator("SBXCrossover",
+				parameters);
 
-	    parameters = new HashMap() ;
-	    parameters.put("probability", 1.0/problem.getNumberOfVariables()) ;
-	    parameters.put("distributionIndex", 20.0) ;
-	    mutation = MutationFactory.getMutationOperator("PolynomialMutation", parameters);                    
+		parameters = new HashMap();
+		parameters.put("probability", 1.0 / problem.getNumberOfVariables());
+		parameters.put("distributionIndex", 20.0);
+		mutation = MutationFactory.getMutationOperator("PolynomialMutation",
+				parameters);
 
-	    // Selection Operator 
-	    parameters = null ;
-	    selection = SelectionFactory.getSelectionOperator("BinaryTournament2", parameters) ;                           
+		// Selection Operator
+		parameters = null;
+		selection = SelectionFactory.getSelectionOperator("BinaryTournament2",
+				parameters);
 
-	    // Add the operators to the algorithm
-	    algorithm.addOperator("crossover",crossover);
-	    algorithm.addOperator("mutation",mutation);
-	    algorithm.addOperator("selection",selection);
+		// Add the operators to the algorithm
+		algorithm.addOperator("crossover", crossover);
+		algorithm.addOperator("mutation", mutation);
+		algorithm.addOperator("selection", selection);
 
-	    // Add the indicator object to the algorithm
-	    algorithm.setInputParameter("indicators", indicators) ;
-	    
-	    // Execute the Algorithm
-	    long initTime = System.currentTimeMillis();
-	    SolutionSet population = algorithm.execute();
-	    long estimatedTime = System.currentTimeMillis() - initTime;
-	    
-	    // Result messages 
-	    logger_.info("Total execution time: "+estimatedTime + "ms");
-	    logger_.info("Variables values have been writen to file VAR");
-	    population.printVariablesToFile("VAR");    
-	    logger_.info("Objectives values have been writen to file FUN");
-	    population.printObjectivesToFile("FUN");
+		// Add the indicator object to the algorithm
+		algorithm.setInputParameter("indicators", indicators);
+		
+		int numberOfRun=2;
+		for (int i = 0; i < numberOfRun; i++) {
+			// Execute the Algorithm
+			long initTime = System.currentTimeMillis();
+			SolutionSet population = algorithm.execute();
+			long estimatedTime = System.currentTimeMillis() - initTime;
 
+			// Result messages
+			logger_.info("Total execution time: " + estimatedTime + "ms");
+			logger_.info("Variables values have been writen to file VAR");
+			population.printVariablesToFile("run_"+i+"_VAR");
+			logger_.info("Objectives values have been writen to file FUN");
+			population.printObjectivesToFile("run_"+i+"_FUN");
+		}
 	}
 }
